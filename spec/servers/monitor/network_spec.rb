cf69e79b3da396d6011dd30016baa3bd490a31f4
Alex Sheverdin <alex@galacticexchange.io>
2017-12-17 19:30:36 +0200

    test - ping ips from array

--- spec/servers/monitor/network_spec.rb
+++ spec/servers/monitor/network_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 require 'aws-sdk'
 
+
 opt = $server_config
 require 'spec_helper'
 
 ##
 API_IP = '46.172.71.54'
+IPs = ['46.172.71.53', '46.172.71.54', '46.172.71.59']
 
-describe 'Сonnect to api on port 80' do
-  describe host(API_IP) do
-    it "Ping" do should be_reachable end
-  end
+describe '-Monitoring nodes-' do
+  IPs.each {|e|
+    describe host(e) do
+      it "is reachable by Ping" do should be_reachable end
+    end
+  }
+  # describe host(API_IP) do
+  #   it "is reachable by Ping" do should be_reachable end
+  # end
 end
